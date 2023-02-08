cd282849f915f646b4df744b587bfddaa7d1593b
qky_ariza <qkyariza@gmail.com>
2023-02-07 19:34:58 -0500

    [CREATE] - Ejemplo de funciones

---
+++ funciones.py
@@ -0,0 +1,17 @@
+def area_rectangulo(
+        x:float,
+        y:float
+) -> float:
+    """
+    Funcion que calcula el area de un rectangulo
+
+    ---params---
+    - x (float): es la base de mi rectangulo
+    - y (float): es la altura de mi rectangulo
+
+    ---return---
+    - area (float): es el area de mi rectangulo
+    """
+area = x*y
+
+area_rectangulo(3,4)
